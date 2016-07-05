0f3926529eb212a97fc5ae827e35bc1f0f2e749f
senjuana <senju5@hotmail.com>
2016-07-04 22:04:17 -0500

    Cambios para hacer commit: 	modificado:    Database/scriptsConsulta.sql 	modificado:    php/conexion.php

--- Database/scriptsConsulta.sql
+++ Database/scriptsConsulta.sql
@@ -8,5 +8,5 @@ INSERT INTO Miembros VALUES(0,'Eduardo','Almeida','Grover',1);
 INSERT INTO Encargados VALUES(0,1,1);
 
 //Consultas\\
-SELECT M.nom,M.ape FROM Miembros AS M ,Proyectos AS P, Encargados AS E where E.Miembroid = M.idMiem;
+SELECT M.nom,M.ape FROM Miembros AS M ,Proyectos AS P, Encargados AS E where E.Miembroid = M.idMiem AND P.idPro = M.Proyectoid;
 //Eliminaciones\\
--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,4 @@
 <?php
-// datos para la coneccion a mysql
 define('DB_SERVER','localhost');
 define('DB_NAME','mydb');
 define('DB_USER','root');
@@ -7,4 +6,4 @@ define('DB_PASS','123');
 
 $con = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
 mysql_select_db(DB_NAME,$con);
-?> 
+?>
